9004c863ef427883ec0c959647c4020aae43cc72
Bikash Chaudhary <bikashchaudhary2063@gmail.com>
2026-08-04 07:23:54 +0545

    Next Day

---
+++ Day-27/script.js
@@ -0,0 +1,64 @@
+// "Use Strict"
+// let x = 5
+// console.log(x)
+
+//scope
+// {
+//     var x = 1
+//     let y = 2
+//     const z = 3
+//     console.log(x, y, z)
+// }
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// let  x = 5
+// console.log(x)
+
+// x = 10
+// console.log(x)
+
+// const x = 5
+// console.log(x)
+
+// x = 10
+// console.log(x)
+
+// let fname = 'Hari'
+// let lname = "Bahadur"
+
+// console.log(fname + lname)
+// console.log("First name is "+fname+" Last name is " + lname)
+
+//case
+
+// snake case = underscore
+
+// let first_name = 'Hari'
+
+// // Pascal Case = First letter capital
+
+// let FirstName = 'Hari'
+
+// // Camel Case 
+
+// let firstNameAndLastName = 'Hari'
+
+// // kebab = hyphen
+
+// let first-name = 'Hari'
+
+// let lastName 
+// lastName = "Chy"
+
+
+// // Should assign value during declaration
+// const Country
+// Country = 'Nepal'
+// console.log(Country)
+
+let x = 5
+let y = 10
+
+console.log("Hello" + (x+y))
