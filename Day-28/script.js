ec841eb280577d8f3fd96267b9ea12127f544bf1
Bikash Chaudhary <bikashchaudhary2063@gmail.com>
2026-08-06 08:28:57 +0545

    Day 2 of Js

---
+++ Day-28/script.js
@@ -0,0 +1,111 @@
+// // operators
+
+// let x = 5
+
+// let y = 10
+// let z = 6
+
+// // Arithmetics operators
+
+// console.log(x+y)
+// console.log(y-x)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(y**x)
+
+// console.log(y/x)
+// console.log(x/y)
+// console.log(y%z)
+// console.log(Math.trunc(y/z))
+
+// // Logical Operators
+
+// // let a = true
+// // let b = false
+
+// // // AND -> &&
+// // // OR -> ||
+// // // NOT -> !
+
+// // console.log(a && b)
+// // console.log(a||b)
+// // console.log(!a)
+
+// // // Comparison Operators
+
+// // let m = 10
+// // let p = 20
+// // let q = "20"
+
+// // // console.log(m==p)
+// // // console.log(m<p)
+// // // console.log(m>p)
+// // // console.log(m<=p)
+// // // console.log(m>=p)
+// // // console.log(m != p)
+
+// // console.log(p == q)
+// // console.log(p === q)
+
+// let a = 15
+// let b = 15
+
+// if(a > b)
+// {
+//     console.log("The statement is true")
+// }
+
+// if(a>b)
+// {
+//    console.log("a is greater") 
+// }
+
+// else{
+//     console.log("b is greater")
+// }
+
+// if(a>b)
+// {
+//     console.log("a is greater")
+// }
+
+// else if (b>a)
+// {
+//     console.log("b is greater")
+// }
+
+// else if(false)
+// {
+//     console.log("It will never execute")
+// }
+
+// else
+// {
+//     console.log("They are equal")
+// }
+
+// Switch statement
+
+// let ticketNumber = 500
+
+// switch(ticketNumber)
+// {
+//     case 100:
+//         console.log("Watch Spiderman")
+//         break
+//     case 200:
+//         console.log("Football")
+//         break
+//     case 300:
+//         console.log("Toxic")
+//         break
+//     default:
+//         console.log("Wrong Movie Hall")
+// }
+
+let a = 5
+let b = 10
+
+// expression ? true statement : false statement
+
+console.log(a>b ? "a is greater" : "b is greater")
