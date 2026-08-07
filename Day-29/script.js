b28e080cfadb2275650ead184c69fe0a9809604c
Bikash Chaudhary <bikashchaudhary2063@gmail.com>
2026-08-07 08:29:48 +0545

    Day-29

---
+++ Day-29/script.js
@@ -0,0 +1,44 @@
+// console.log("Hello")
+
+// for loop
+// for(initialization; condition; increment/decrement){
+//     // statement
+// }
+
+// let x = 5
+// console.log(x++)
+// console.log(x)
+    
+// let x
+// for(x = 0; x<5; x++)
+// {
+//     console.log(x)
+// }
+
+// while(conditon){statement}
+
+// 
+
+// do{statement} while(condition)
+
+// let x = 1
+// do{
+//     console.log(x)
+//     x++
+// }
+// while(x<6)
+
+// varaible declare, print "Hello" as many as varaible size
+
+let x = 25
+for(i=0; i<x; i++)
+{
+    console.log("Hello")
+}
+
+let y = 0
+while(y<x)
+{
+    console.log("Hello")
+    y++
+}
