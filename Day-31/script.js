f087d0363d8eb25446a56d6e1bab16aff66363e8
Bikash Chaudhary <bikashchaudhary2063@gmail.com>
2026-08-17 08:30:05 +0545

    Updated

---
+++ Day-31/script.js
@@ -0,0 +1,18 @@
+let a = 10
+if(a%2==0)
+{
+    console.log("A")
+}
+else if(a%3==0)
+{
+    console.log("B")
+}
+else if(a%2==0 && a%3==0)
+{
+    console.log("C")
+}
+else
+{
+    console.log("D")
+}
+
---
+++ Day-32/script.js
@@ -0,0 +1,47 @@
+//   console.log('first')
+
+//   function myFunction()
+// {
+//     console.log("Hello Function")
+// }
+
+// myFunction()
+
+// // int, void, string
+
+// function giveValue()
+// {
+//     return 5
+// }
+// let x = giveValue()
+// console.log(x)
+
+// Parameters
+// function sum(a,b)
+// {
+//     console.log("sum " + (a,b))
+// }
+// // argument
+// // sum(2,3)
+
+// printNumber(10)
+// function printNumber(a = 5)
+// {
+//     console.log("a is " +a)
+// }
+
+// Create a function that takes two
+// numbered parameters 
+// create a loop inside function
+// and print all the numbers from first parameter to
+// second
+
+function printNumbers(a,b)
+{
+    for(let i = a; i<=b; i++)
+    {
+        console.log(i)
+    }
+    
+}
+printNumbers(1,25)
